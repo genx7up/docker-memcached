52f38540a5d19c58aada50e9d61d8aaaa353b7e8
genx7up <genx7up@users.noreply.github.com>
2015-04-28 21:47:25 -0700

    bump to 1.4.24

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #Build
 echo "%_topdir /home/you/rpmbuild" >> ~/.rpmmacros
 mkdir -p /home/you/rpmbuild/{SPECS,BUILD,SRPMS,RPMS,SOURCES}
-rpmbuild -ta memcached-1.4.20.tar.gz
+rpmbuild -ta memcached-1.4.24.tar.gz
 
 #Copy the new binary to shared volume
-cp /home/you/rpmbuild/RPMS/x86_64/memcached-1.4.20-1.el6.x86_64.rpm /data/
+cp /home/you/rpmbuild/RPMS/x86_64/memcached-1.4.24-1.el6.x86_64.rpm /data/
